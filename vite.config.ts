4c5ef7e539ca08223ba211f2ab3a7ea29ab7e2df
Anshul  choure <139856005+Anshul-251204@users.noreply.github.com>
2024-05-11 12:25:24 +0530

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,11 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [react()],
-<<<<<<< HEAD
-	// server: {
-	// 	proxy: {
-	// 		"/api": "https://twitter-node-prisma-2.onrender.com/",
-	// 	},
-	// },
-=======
 	server: {
-		proxy: {
-			"/api": "https://twitter-node-prisma-2.onrender.com",
-		},
+		// proxy: {
+		// 	"/api": "https://twitter-node-prisma-2.onrender.com",
+		// },
 	},
->>>>>>> 11b0f19f2203c1b6bb662e9a64048468328186d5
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
